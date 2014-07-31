5cf4b958e52733dd7026a791630ddd39c519c354
Cory Bevilacqua <Cory@Corys-MacBook-Pro.local>
2014-07-31 02:58:07 -0400

    Initial Commit

---
+++ Stable Partition/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Stable Partition
+//
+//  Created by Cory Bevilacqua on 7/31/14.
+//  Copyright (c) 2014 Cory Bevilacqua. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
